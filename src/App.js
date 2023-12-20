1e0026c9dbbc2ff8a737b1d903505706240efb18
cotasi <cotasi135@gmail.com>
2023-12-20 10:35:52 +0900

    usestate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[yo,youpdate] = useState(0);
+  const yolist=['월','화','수','목','금','토','일'];
+
+  const updateyo = ()=>{
+    let count;
+    count = yo + 1;
+    count %= 7;
+    youpdate(count);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>{ updateyo(); }}>클릭하면 아래 요일이 하나씩 수정되게 해주세요.</button>
+      <p>{yolist[yo]}요일</p>
     </div>
   );
 }
